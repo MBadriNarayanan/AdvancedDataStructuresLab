551365ee64038863e2bf78079e4a686c5373b181
MBadriNarayanan <58718023+MBadriNarayanan@users.noreply.github.com>
2019-12-31 12:29:59 +0530

    Matrix Header File

---
+++ matrix.h
@@ -0,0 +1,23 @@
+#ifndef matrix_h
+#define matrix_h
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+class Matrix
+{
+	public :
+		
+		int 	**data;
+		int 	r,c;
+		int 	input();
+		int 	display();
+		Matrix 	add(Matrix b);
+		Matrix  sub(Matrix b);
+		Matrix  mul(Matrix b);
+		Matrix  transpose();
+		Matrix();
+		Matrix(int row,int column);
+		~Matrix();
+};
+#endif	
