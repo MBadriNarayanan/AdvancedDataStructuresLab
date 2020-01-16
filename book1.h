299d77ad61ea138c8ee43593a54c3ced1807274e
MBadriNarayanan <58718023+MBadriNarayanan@users.noreply.github.com>
2020-01-16 15:16:38 +0530

    User Defined Class Using Friend Class And Operator Overloading Header File

---
+++ book1.h
@@ -0,0 +1,42 @@
+#ifndef book1_h
+#define book1_h
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+#include<string.h>
+
+
+using namespace std;
+
+class BookDetails
+{
+	private:
+		char 	name[30];
+		char	author[30];
+		float 	price;
+		int 	bookno;
+		char	publisher[30];
+		int 	publishernumber;
+	public :
+	        
+	friend  class   Library;
+	friend  ostream& operator <<(ostream&,BookDetails);
+	friend  istream& operator >>(istream&,BookDetails&);
+};
+class Library
+{
+	private :
+			BookDetails *data;
+			int 	size;
+	public  :
+			int 	input();
+			int 	display();
+			int 	sort();
+			int     insert();
+			int 	delete1();
+			int 	search(int bookno1);
+			Library();
+			Library(int n);
+			//~Library();
+};
+#endif
