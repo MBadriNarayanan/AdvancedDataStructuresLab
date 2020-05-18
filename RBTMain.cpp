2966307dc7b0f73e27c5778b901776193d112848
MBadriNarayanan <58718023+MBadriNarayanan@users.noreply.github.com>
2020-05-18 10:23:53 +0530

    RBT Main File

--- RBTMain.cpp
+++ RBTMain.cpp
@@ -17,29 +17,7 @@ int main()
 		cin>>choice;
 	}while(choice);
 	display_bst(b.getRoot());
-	b.display();
-	cout<<"\n\n\n";
-	/*do
-	{
-		cout<<" Enter Data To Be Searched : ";
-		cin>>data;
-		b.search(data);
-		cout<<" Do You Have Any More Data To Be Deleted (Type 0 Or 1) : ";
-		cin>>choice;
-	}while(choice);
-	display_bst(b.getRoot());
-	b.display();
-	cout<<"\n\n\n";
-	do
-	{
-		cout<<" Enter Data To Be Deleted : ";
-		cin>>data;
-		b.delete_key(data);
-		cout<<" Do You Have Any More Data To Be Deleted (Type 0 Or 1) : ";
-		cin>>choice;
-	}while(choice);
-	cout<<" Displaying The Deleted Tree \n\n";
-	display_bst(b.getRoot());*/			
+	b.display();	
 	cout<<"\n\n\n The End \n\n\n";
 	return 0;
 }
